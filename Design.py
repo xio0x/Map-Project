cd92e61ca7fba36acbfc78f6ad213ddfa44d1176
Joyal-Shaji <80173348+Joyal-Shaji@users.noreply.github.com>
2024-03-05 21:21:39 -0500

    Update Design.py
    added my signs

--- Design.py
+++ Design.py
@@ -92,3 +92,81 @@ def railroad():
         forward(50)
 
 #railroad()
+
+def no_vehicles(x):
+    # draws a circle for the sign and fills it in
+    speed(0)
+    goto(0, -50 * (2 * x))
+    pensize(30 * x)
+    pendown()
+    color("#c1111b", "white")
+    begin_fill()
+    circle(radius=100 * x, extent=None, steps=200 * x)
+    end_fill()
+
+#no_vehicles(3)
+
+def railroad_crossing():
+    speed(4)
+
+    def sign_background():
+        # code for the background and border of the sign
+        penup()
+        goto(-50, -50)
+        pendown()
+        color("black", "white")
+        begin_fill()
+        pensize(5)
+        forward(100)
+        left(90)
+        forward(350)
+        left(90)
+        forward(100)
+        left(90)
+        forward(350)
+        end_fill()
+
+    def red_strip():
+        # code for the red strip and fills it in
+        penup()
+        color("#c1111b", "#c1111b")
+        begin_fill()
+        goto(-50, 40)
+        pendown()
+        goto(50, 90)
+        left(180)
+        forward(40)
+        goto(-50, 80)
+        right(180)
+        forward(40)
+        end_fill()
+
+        # code that outlines the shape in a black line to make it neater
+        penup()
+        goto(-50, -50)
+        left(90)
+        pendown()
+        color("black")
+        pensize(5)
+        forward(100)
+        left(90)
+        forward(350)
+        left(90)
+        forward(100)
+        left(90)
+        forward(350)
+
+    def writing():
+        #   code for the 50 m writing
+        penup()
+        color("black")
+        goto(0, 255)
+        write("50", True, align="center", font=("Lexend", 30, "normal"))
+        goto(0, 230)
+        write(arg="m", move=True, align="center", font=("Lexend", 30, "normal"))
+
+    sign_background()
+    red_strip()
+    writing()
+    ht()
+#railroad_crossing():
