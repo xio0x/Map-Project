fee87bc5ed493ab78687a895c4fb88b88b9657cf
Charlton <160947165+Enderpuff1@users.noreply.github.com>
2024-03-09 17:41:23 -0500

    Update Design.py

--- Design.py
+++ Design.py
@@ -1,10 +1,52 @@
+import turtle
 from turtle import *
-setup(1000,1000)
+setup(1500,1000)
 speed(0)
+global inMap
+inMap = False
+#DANGER SIGN
 
-#danger
+def danger(x):
+
+    #setting turtle
+
+    penup()
+    setheading(0*x)
+    back(100*x)
+    pencolor("Red")
+    pendown()
+    width(15*x)
+
+    #triangle
+    forward(200*x)
+    setheading(120)
+    forward(200*x)
+    setheading(240)
+    forward(200*x)
+
+    #Exclamation Mark
+    penup()
+    setheading(0)
+    forward(100*x)
+    setheading(90)
+    forward(30*x)
+    pendown()
+    pencolor("black")
+    forward(1*x)
+    penup()
+    forward(25*x)
+    pendown()
+    forward(50*x)
+    penup()
+
+#END OF DANGER SIGN
+
+
+
+#CURVELEFT SIGN
+
 def curveLeft(x):
-    #setting up
+    # setting up
 
     penup()
     setheading(0)
@@ -12,9 +54,9 @@ def curveLeft(x):
     back(100 * x)
     pencolor("red")
     pendown()
-    width(15 * x)
+    width(15*x)
 
-    #triangle
+    # triangle
 
     forward(200 * x)
     setheading(120)
@@ -22,161 +64,160 @@ def curveLeft(x):
     setheading(240)
     forward(200 * x)
 
-    #Curvy guy
+    # Curvy guy
 
     penup()
     color("black")
     setheading(0)
-    forward((125) * x)
+    forward(125*x)
     setheading(90)
-    forward(30 * x)
+    forward(30*x)
     pencolor("black")
     width(1)
     setheading(0)
-    forward(10)
+    forward(10*x)
     pendown()
     begin_fill()
     setheading(90)
     pos = position()
-    circle(50,90)
-    setheading(180+45)
-    forward(20)
-    setheading(360-45)
-    forward(20)
+    circle(50, 90)
+    setheading(180 + 45)
+    forward(20*x)
+    setheading(360 - 45)
+    forward(20*x)
     pos2 = position()
     setheading(360)
     penup()
     goto(pos)
-    setheading(90+45)
+    setheading(90 + 45)
     pendown()
-    forward(20)
-    setheading(270-45)
-    forward(20)
+    forward(20*x)
+    setheading(270 - 45)
+    forward(20*x)
     setheading(90)
-    circle(25, 90)
+    #the circle's radious should be altered but it isn't :(
+    circle(25*x, 90*x)
     end_fill()
     penup()
-    forward(200)
 
-    #stop
-    
+# END OF CURVELEFT SIGN
+
+
+#STOP SIGN
+
+def stopsign(x):
     pendown()
-    color('white')
+    color('red')
     fillcolor('red')
     begin_fill()
     for i in range(8):
-            left(45)
-            forward(50)
+        left(45)
+        forward(50*x)
     end_fill()
     penup()
-    goto(-38,-8)
-    right(120)
-    left(70)
-    forward(-55)
-    backward(5)
-    color('white')
-    write('STOP', font=('Arial', 40, 'normal', 'bold'))
-    hideturtle()
-    
-    #circle
-    
-    y = 50
+    if inMap == False:
+        goto(-38, -8)
+        right(120)
+        left(70)
+        forward(-55)
+        backward(5)
+        color('white')
+        write('STOP', font=('Arial', 30, 'normal', 'bold'))
+    elif inMap == True:
+        setheading(90)
+        forward(-100 * x)
+        setheading(0)
+        forward(-220 * x)
+        color('white')
+        pendown()
+        write('STOP', font=('Arial', 20, 'normal', 'bold'))
+        penup()
+
+#END OF STOP SIGN FUNCTION
+
+
+#BUS STOP SIGN
+
+def busStop(x):
+    setheading(90)
     pendown()
     pensize(3)
     color('#FFD700')
     fillcolor('#228B22')
     begin_fill()
-    circle(x)
+    circle(70*x)
     end_fill()
     penup()
-    goto(0,20)
     pendown()
+    setheading(0)
+    forward(-15*x)
+    setheading(90)
     color('#FFD700')
     fillcolor('#FFD700')
     begin_fill()
-    circle(y)
+    circle(55*x)
     end_fill()
     penup()
-    hideturtle()
-    goto(-31,19)
+    setheading(0)
+    forward((-70*x - 25*x))
+    setheading(90)
+    forward(-70*x)
     color('#228B22')
-    write('H', font=('Arial', 90, 'normal'))
-
-#curveLeft(1)
+    write('H', font=('Arial', int(90*x), 'normal'))
 
 
+#END OF BUS STOP FUNCTION
 
-def railroad():
-    penup()
-    goto(-700,-400)
-    pencolor("gray")
-    width(10)
-    setheading(90)
-    forward(1000)
-    penup()
-    setheading(0)
-    forward(100)
-    setheading(270)
-    pendown()
-    forward(100)
-    penup()
-    goto(-700, -400)
-    setheading(90)
-    forward(30)
-    penup()
-    setheading(0)
-    back(30)
-    for i in range (0, (20)):
-        setheading(0)
-        pos = position()
-        pendown()
-        forward(130)
-        penup()
-        goto(pos)
-        setheading(90)
-        forward(50)
-
-#railroad()
+#NO VEHICLES SIGN
 
 def no_vehicles(x):
     # draws a circle for the sign and fills it in
-    speed(0)
-    goto(0, -50 * (2 * x))
-    pensize(30 * x)
+    setheading(90)
+    forward(-50*x)
+    pensize(30*x)
     pendown()
     color("#c1111b", "white")
     begin_fill()
-    circle(radius=100 * x, extent=None, steps=200 * x)
+    circle(radius=int(100 * x), extent=None, steps=int(200 * x))
     end_fill()
+    penup()
 
-#no_vehicles(3)
+#END OF NO VEHICLES SIGNa
 
-def railroad_crossing():
-    speed(4)
+#RAILROAD CROSSING SIGN
 
-    def sign_background():
+def railroad_crossing(x):
+    speed(0)
+    def sign_background(x):
         # code for the background and border of the sign
         penup()
-        goto(-50, -50)
+        setheading(0)
+        forward(-50*x)
+        setheading(90)
+        forward(-50*x)
         pendown()
         color("black", "white")
         begin_fill()
         pensize(5)
-        forward(100)
+        setheading(0)
+        forward(100*x)
         left(90)
-        forward(350)
+        forward(350*x)
         left(90)
-        forward(100)
+        forward(100*x)
         left(90)
-        forward(350)
+        forward(350*x)
         end_fill()
 
-    def red_strip():
+    def red_strip(x):
         # code for the red strip and fills it in
         penup()
         color("#c1111b", "#c1111b")
         begin_fill()
-        goto(-50, 40)
+        setheading(0)
+        forward(-50*x)
+        setheading(90)
+        forward(40*x)
         pendown()
         goto(50, 90)
         left(180)
@@ -185,7 +226,7 @@ def railroad_crossing():
         right(180)
         forward(40)
         end_fill()
-
+    '''
         # code that outlines the shape in a black line to make it neater
         penup()
         goto(-50, -50)
@@ -200,7 +241,7 @@ def railroad_crossing():
         forward(100)
         left(90)
         forward(350)
-
+    '''
     def writing():
         #   code for the 50 m writing
         penup()
@@ -210,19 +251,16 @@ def railroad_crossing():
         goto(0, 230)
         write(arg="m", move=True, align="center", font=("Lexend", 30, "normal"))
 
-    sign_background()
-    red_strip()
+    sign_background(x)
+    red_strip(x)
     writing()
     ht()
-#railroad_crossing():
 
-#    Code for the map
-
-setup(1500,1000)
-screensize(None, None, "#4ce32d")
-speed(0)
+#END OF RAILROAD CROSSING SIGN
 
 
+
+#FUNCTION THAT CREATES MAP
 def map_design():
 
     def railroad():
@@ -230,16 +268,17 @@ def map_design():
 
         penup()
         goto(-700, -400)
-        pencolor("brown")
+        pencolor("grey")
         width(10)
         setheading(90)
+        pendown()
         forward(1000)
         penup()
         setheading(0)
-        forward(100)
+        forward(75)
         setheading(270)
         pendown()
-        forward(100)
+        forward(1000)
         penup()
         goto(-700, -400)
         setheading(90)
@@ -247,6 +286,7 @@ def map_design():
         penup()
         setheading(0)
         back(30)
+        pencolor("brown")
         for i in range(0, 16):
             setheading(0)
             pos = position()
@@ -304,7 +344,100 @@ def map_design():
     circle(300, 90)
     ht()
 
-    railroad()
+
+#END OF FUNCTION THAT CREATES MAP
+
+#FUNCTION TO MAKE SIGNS ON MAP
+
+def mapSigns():
+    penup()
+    goto(40,-110)
+    stopsign(0.2)
+    goto(200, 30)
+    danger(.4)
+    goto(240,-110)
+    busStop(.5)
+    goto(140,240)
+    curveLeft(.4)
+    goto(-500,90)
+    no_vehicles(.3)
+    goto(-530, -380)
+'''
+    railroad_crossing(.2)
+'''
+#END FUNCTION TO MAKE SIGNS ON MAP
+#RAILROAD CROSSING DOESNT WORK
+
+#MENU
+
+while True:
+    print("Select and Option:")
+    print("Display Map [1]")
+    print("Display Specific Sign [2]")
+    print("Exit [3]")
+    decision = int(input())
+    if decision == 1:
+        turtle.Screen().reset()
+        inMap = True
+        speed(0)
+        map_design()
+        mapSigns()
+    elif decision == 2:
+        print("Select and Option:")
+        print("Danger [1]")
+        print("Curve Left [2]")
+        print("Bus/Tram Stop [3]")
+        print("Railroad Crossing [4]")
+        print("No Vehicles of Any Kind Permitted [5]")
+        print("Stop [6]")
+        print("Exit [7]")
+        decision2 = int(input())
+        if decision2 == 1:
+            penup()
+            goto(0,0)
+            turtle.Screen().reset()
+            speed(0)
+            danger(1)
+        elif decision2 == 2:
+            penup()
+            goto(0, 0)
+            turtle.Screen().reset()
+            speed(0)
+            curveLeft(1)
+        elif decision2 == 3:
+            penup()
+            goto(0, 0)
+            turtle.Screen().reset()
+            speed(0)
+            busStop(1)
+        elif decision2 == 4:
+            penup()
+            goto(0, 0)
+            turtle.Screen().reset()
+            speed(0)
+            railroad_crossing(1)
+        elif decision2 == 5:
+            penup()
+            goto(0, 0)
+            turtle.Screen().reset()
+            speed(0)
+            no_vehicles(1)
+        elif decision2 == 6:
+            penup()
+            inMap = False
+            goto(0, 0)
+            turtle.Screen().reset()
+            speed(0)
+            stopsign(1)
+
+    elif decision == 3:
+        exit()
+False
+
+#END OF MENU
+
+Turtle._screen.exitonclick()
+exit()
 
 
-#map_design()
+
